62ad5d67e6e88a4de524d966b19cfafb976a1377
Bestbeedev <josueaoga0@gmail.com>
2026-03-06 21:11:37 +0100

    import require removed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import path from 'path';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
@@ -10,14 +8,6 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    css: {
-      postcss: {
-        plugins: [
-          tailwindcss(),
-          autoprefixer(),
-        ],
-      },
-    },
     server: {
       port: 5173,
       host: true,
