9b68a8f0923e4c552e1a9e67a2a0e3e1084538ab
stephdeve <stephdev6@gmail.com>
2026-03-12 12:42:11 +0100

    add of some rests pages

---
+++ react-native/smartqueue/app/about.tsx
@@ -0,0 +1,5 @@
+import AboutAppScreen from '../src/screens/profile/AboutAppScreen';
+
+export default function AboutPage() {
+  return <AboutAppScreen />;
+}
---
+++ react-native/smartqueue/app/personal-info.tsx
@@ -0,0 +1,5 @@
+import PersonalInfoScreen from '../src/screens/profile/PersonalInfoScreen';
+
+export default function PersonalInfoPage() {
+  return <PersonalInfoScreen />;
+}
