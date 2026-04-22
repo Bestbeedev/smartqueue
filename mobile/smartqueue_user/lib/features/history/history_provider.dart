5511db68d821d3ed5e0511fc744792fec6025acf
Bestbeedev <josueaoga0@gmail.com>
2026-04-22 15:47:57 +0100

    cleanup and architecture purged

--- mobile/smartqueue_user/lib/features/history/history_provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../data/api_client.dart';
-import '../../data/repositories/tickets_repository.dart';
-
-final historyTicketsProvider = FutureProvider.autoDispose((ref) async {
-  final api = await ApiClient.create();
-  final repo = TicketsRepository(api);
-  return repo.history(perPage: 50);
-});
